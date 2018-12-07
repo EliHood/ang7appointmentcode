2b0786a6c76a658f4afc69740593c587e460abc5
POWER USER <poweruser@Users-iMac-2.local>
2018-12-07 12:38:41 -0500

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+      apiKey: "AIzaSyBwZehdFCSEObRUsvcNeLtauNxu0vajAJ4",
+      authDomain: "angschedule-59269.firebaseapp.com",
+      databaseURL: "https://angschedule-59269.firebaseio.com",
+      projectId: "angschedule-59269",
+      storageBucket: "angschedule-59269.appspot.com",
+      messagingSenderId: "258175519922"
+  }
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase:{
       apiKey: "AIzaSyBwZehdFCSEObRUsvcNeLtauNxu0vajAJ4",
       authDomain: "angschedule-59269.firebaseapp.com",
